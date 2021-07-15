0a47d973b48f4379bde42515ad56261bf6d96d29
Jaewoon Shim <jwun95@naver.com>
2021-07-15 16:47:42 +0900

    심재운 - course 작업

--- course/urls.py
+++ course/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('update_course/<int:pk>/', views.CourseUpdate.as_view()),
+    path('create_course/', views.CourseCreate.as_view()),
     path('', views.CourseList.as_view()),
+    path('<int:pk>/', views.CourseDetail.as_view()),
 ]
