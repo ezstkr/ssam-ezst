4e3f28c8a1be973a654b86e940650e1b1a37f9e3
aiacademy131 <aiacademy131@gmail.com>
2021-07-15 15:19:46 +0900

    .gitignore 수정

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Lecture
 
 
 admin.site.register(Course)
+
+admin.site.register(Lecture)
--- course/models.py
+++ course/models.py
@@ -1,5 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Lecture(models.Model):
+    creator = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    head_image = models.ImageField(upload_to='course/images/%Y/%m/%d/', blank=True)
+
+    slide = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title} :: {self.creator}'
 
 
 class Course(models.Model):
+    creator = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    head_image = models.ImageField(upload_to='course/images/%Y/%m/%d/', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    lectures = models.ManyToManyField(Lecture, blank=True)
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title} :: {self.creator}'
+
+
