190c9d0f3ec380ffe2d0c7146ed830d7801fa160
star14ms <danalstj@naver.com>
2021-07-16 15:48:36 +0900

    Lecture create, detail 베이스 완성

--- lecture/models.py
+++ lecture/models.py
@@ -16,7 +16,7 @@ class Lecture(models.Model):
     def __str__(self):
         return f'[{self.pk}] {self.title}'
 
-    def get_url(self):
+    def get_absolute_url(self):
         return f'/lecture/{self.pk}/'
 
     def get_file_name(self):
--- lecture/urls.py
+++ lecture/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = 'lecture'
 
 urlpatterns = [
-    path('<int:pk_course>/', views.lecture_list),
+    path('list/<int:pk_course>/', views.lecture_list),
+    path('<int:pk_lecture>/', views.lecture_detail),
     path('create/', views.LectureCreate.as_view()),
 #     path('update/', views.), # 개발 대기 중
-#     path('<int:pk>/', views.),
+
+    path('', views.lecture_list),
+    path('list/', views.lecture_list),
 ]
 
 # path('<int:pk_course>/', views.LectureList.as_view(), name='list'),
