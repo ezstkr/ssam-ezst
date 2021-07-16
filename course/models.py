decb5386a5bfb4debf6d2567cbcfc3fff1d175c2
Jaewoon Shim <jwun95@naver.com>
2021-07-16 12:00:39 +0900

    강의 코스 조회 생성구현 -심재운

--- course/models.py
+++ course/models.py
@@ -18,4 +18,7 @@ class Course(models.Model):
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.creator}'
 
+    def get_absolute_url(self):
+        return f'/course/{self.pk}/'
+
 
